ea233d09a11952be7a6158584fadbeb01246388c
ansarirayyan01 <ansarirayyan874@gmail.com>
2024-07-25 01:43:25 +0530

    created user schema

---
+++ backend/db.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+mongoose.connect("mongodb://localhost:27017/paytm")
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+})
+
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true,
+    }
+})
+
+const User = mongoose.model("User", userSchema)
+const Account = mongoose.model("Account", accountSchema)
+
+module.exports = { User, Account }
